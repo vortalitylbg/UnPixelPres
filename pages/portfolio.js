65aceaa4a06246f50eed4f779ebbd6049c417871
Palacraft <99590109+vortalitylbg@users.noreply.github.com>
2025-06-29 15:40:36 +0200

    Update portfolio.js

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -1,31 +1,31 @@
 const projects = [
   {
     title: "Bac Marée | St Aubin La salle",
-    image: "/images/sites_portfolio/bac_a_maree.png",
+    image: "https://github.com/vortalitylbg/WebSurMesure/blob/main/images/sites_portfolio/bac_a_maree.png?raw=true",
     description: "Prototype d'un site web pour une micro-entreprise lycéenne engagée dans la collecte et la valorisation des déchets plastiques sauvages, dans le cadre d’un projet Bac en section euro plastique & composite.",
     link: "https://vortalitylbg.github.io/Projet_bac_a_maree/"
   },
   {
     title: "Exemple Site 1",
-    image: "/images/sites_portfolio/temp.png",
+    image: "https://github.com/vortalitylbg/WebSurMesure/blob/main/images/sites_portfolio/temp.png?raw=true",
     description: "Pas encore de site pour cet emplacement.",
     link: "#"
   },
   {
     title: "Exemple Site 2",
-    image: "/images/sites_portfolio/temp.png",
+    image: "https://github.com/vortalitylbg/WebSurMesure/blob/main/images/sites_portfolio/temp.png?raw=true",
     description: "Pas encore de site pour cet emplacement.",
     link: "#"
   },
   {
     title: "Exemple Site 3",
-    image: "/images/sites_portfolio/temp.png",
+    image: "https://github.com/vortalitylbg/WebSurMesure/blob/main/images/sites_portfolio/temp.png?raw=true",
     description: "Pas encore de site pour cet emplacement.",
     link: "#"
   },
   {
     title: "Exemple Site 4",
-    image: "/images/sites_portfolio/temp.png",
+    image: "https://github.com/vortalitylbg/WebSurMesure/blob/main/images/sites_portfolio/temp.png?raw=true",
     description: "Pas encore de site pour cet emplacement.",
     link: "#"
   }
