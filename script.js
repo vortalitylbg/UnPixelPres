e80f5a0a681ff3d2161058568b7e196b9d6f3e42
Palacraft <99590109+vortalitylbg@users.noreply.github.com>
2025-07-06 11:50:57 +0200

    Update script.js

--- script.js
+++ script.js
@@ -11,5 +11,35 @@ function revealOnScroll() {
   });
 }
 
+
 window.addEventListener('scroll', revealOnScroll);
 window.addEventListener('load', revealOnScroll);
+
+
+const hamburger = document.querySelector('.hamburger');
+const hamburgerIcon = hamburger.querySelector('i');
+const nav = document.querySelector('.nav');
+const navLinks = document.querySelectorAll('.nav-links a');
+
+hamburger.addEventListener('click', () => {
+  nav.classList.toggle('open');
+  hamburger.classList.toggle('active');
+
+  // Toggle icône Font Awesome
+  if (nav.classList.contains('open')) {
+    hamburgerIcon.classList.remove('fa-bars');
+    hamburgerIcon.classList.add('fa-xmark');
+  } else {
+    hamburgerIcon.classList.remove('fa-xmark');
+    hamburgerIcon.classList.add('fa-bars');
+  }
+});
+
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    nav.classList.remove('open');
+    hamburger.classList.remove('active');
+    hamburgerIcon.classList.remove('fa-xmark');
+    hamburgerIcon.classList.add('fa-bars');
+  });
+});
